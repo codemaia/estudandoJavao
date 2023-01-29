bb37ded5f1ed5f7dadc554a868a8b1941557ffc2
Renan Maia <renanmaia.rl@gmail.com>
2023-01-29 00:50:49 -0300

    exercise vetores in JAVA. GOD IS GOOD

--- src/application/ProgramExerciseVetores.java
+++ src/application/ProgramExerciseVetores.java
@@ -1,12 +1,13 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ProgramExerciseVetores {
 
 	public static void main(String[] args) {
 		
-		 
+		Locale.setDefault(Locale.US);
 		Scanner sc =  new Scanner(System.in);
 		
 		//PROBLEMA "NEGATIVOS"
@@ -41,8 +42,65 @@ public class ProgramExerciseVetores {
 		
 		
 		//PROBLEMA "SOMA_VETOR"
+		/*
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int n = sc.nextInt();
 		
-		 
+		double[] vect = new double[n];
+		
+		for (int i = 0; i < vect.length; i++) {
+			System.out.print("Digite um numero: ");
+			vect[i] = sc.nextDouble();
+		}
+		
+		
+		double sum = 0.0;
+		//double avg;
+		System.out.print("VALORES = " );
+		for (int i = 0; i < vect.length; i++) {
+			System.out.print(vect[i] + " ");
+			sum += vect[i];
+		}
+		System.out.println();
+		System.out.print("SOMA = " + sum);
+		System.out.println();
+		
+		double avg = sum / vect.length;
+		System.out.print("MEDIA = " + avg);
+		*/
+		
+		
+		
+		//PROBLEMA "ALTURAS"
+		
+		int n;
+		
+		System.out.print("Quantas pessoas sera digitadas? ");
+		n = sc.nextInt();
+		
+		String[] vectName = new String[n];
+		int[] 	 vectIdade = new int[n];
+		double[] vectAltura = new double[n];
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println("Dados da " + (i + 1) + "a pessoa:");
+			sc.nextLine();
+			
+			System.out.print("Name: ");
+			vectName[i] = sc.nextLine();
+			//sc.nextLine();
+			
+			System.out.print("Idade: ");
+			vectIdade[i] = sc.nextInt();
+			
+			System.out.print("Altura: ");
+			vectAltura[i] = sc.nextDouble();
+		}
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println("result: "+ vectName[i] + ", " + vectIdade[i] + ", "+ vectAltura[i]);
+		}
+		
 		
 		sc.close();
 
