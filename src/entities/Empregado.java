647ce907a5b49fed930415a1bc294e9a7acdd624
Renan Maia <renanmaia.rl@gmail.com>
2023-02-04 00:01:18 -0300

    arrays in JAVA and OO. GOD IS GOODgit push origin main

--- src/entities/Empregado.java
+++ src/entities/Empregado.java
@@ -4,7 +4,45 @@ public class Empregado {
 	
 	private Integer id;
 	private String name;
-	private Double Salary;
+	private Double salary;
+	
+	
+	
+	public Empregado(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(Double salary) {
+		this.salary =  salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Empregado [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
 	
 	
 }
