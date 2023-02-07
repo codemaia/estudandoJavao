99ca2626f10d9b0d90bb636b4f906441ee9549b7
Renan Maia <renanmaia.rl@gmail.com>
2023-02-06 23:03:07 -0300

    Data and hour in JAVA. THANK YOU GOD!

--- src/resumos/DataHora.java
+++ src/resumos/DataHora.java
@@ -1,5 +1,9 @@
 package resumos;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -40,7 +44,60 @@ public class DataHora {
 	 * 	- Data-hora +/- tempo -> Data-hora
 	 * 	- Data-hora 1, Data-hora 2 -> Duração 
 	 * 
+	 * 
+	 *  #PRINCIPAIS TIPOS
+	 *  
+	 *  @Data-hora local
+	 *   - LocalDate
+	 *   - LocalDateTime
+	 *   
+	 *  @Data-hora globl
+	 *   - Instant
+	 *   
+	 *   
+	 *  @Duração
+	 *   - Duration
+	 *   
+	 *  @Outros
+	 *   - ZoneId
+	 *   - ChronoUnit
+	 * 
+	 * 
 	 */
+
+		// INSTANCIANDO LOCALDATE
+		
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		LocalDate d01 = LocalDate.now();
+		LocalDateTime d02 =  LocalDateTime.now();
+		Instant d03 = Instant.now();
+		
+		LocalDate d04 = LocalDate.parse("2023-02-07");
+		LocalDateTime d05 = LocalDateTime.parse("2023-02-07T09:30:30");
+		Instant d06 = Instant.parse("2023-02-07T09:30:30Z");
+		Instant d07 = Instant.parse("2023-02-07T09:30:30-03:00");
+		
+		LocalDate d08 = LocalDate.parse("01/01/2023", fmt1);
+		LocalDateTime d09 = LocalDateTime.parse("01/01/2023 00:35", fmt2);
+		LocalDate d10 = LocalDate.of(2023, 02, 07);
+		LocalDateTime d11 = LocalDateTime.of(2033, 02, 07, 1, 30);
+		
+		System.out.println("d01 = " + d01);
+		System.out.println("d02 = " + d02);
+		System.out.println("d03 = " + d03);
+		System.out.println("d04 = " + d04);
+		System.out.println("d05 = " + d05);
+		System.out.println("d06 = " + d06);
+		System.out.println("d07 = " + d07);
+		System.out.println("d08 = " + d08);
+		System.out.println("d09 = " + d09);
+		System.out.println("d10 = " + d10);
+		System.out.println("d11 = " + d11);	
+		
+		
+		
 		
 		
 		
