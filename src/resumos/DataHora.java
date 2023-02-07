4d1ad5ef515a2277595dc2013895920b3fdcc26e
Renan Maia <renanmaia.rl@gmail.com>
2023-02-06 22:19:48 -0300

    Data and hour in JAVA. THANK YOU GOD!

---
+++ src/resumos/DataHora.java
@@ -0,0 +1,52 @@
+package resumos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DataHora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		//OPERAÇOES IMPORTANTES COM DATA-HORA
+		
+	/*
+	 * # INSTANCIAÇÃO
+	 *  - (agora) -> Data-hora
+	 *  - Texto ISO 8601 -> Data-hora
+	 *  - Texto formato customizado - Data-hora
+	 *  - dia, mes, ano, [horario] -> Data-hora local
+	 * 
+	 * 
+	 * # FORMATACAO
+	 *  - Data-hora -> Texto ISO 8601
+	 *  - Data-hora -> Texto formato customizado
+	 * 
+	 * 
+	 * 
+	 * # OBTER DADOS DE UMA DATA-HORA LOCAL
+	 *  - Data-hora local -> dia, mês, ano, horario
+	 *  
+	 *  
+	 * # CONVERTER DATA-HORA GLOBAL PARA LOCAL
+	 * 	- Data-hora global, timezone (sistema local) -> Data-hora local
+	 * 
+	 *  
+	 * # Cálculos com data-hora
+	 * 	- Data-hora +/- tempo -> Data-hora
+	 * 	- Data-hora 1, Data-hora 2 -> Duração 
+	 * 
+	 */
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
