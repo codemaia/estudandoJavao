bbecbf122095b0d5be33cfe55026243582aaa5eb
Renan Maia <renanmaia.rl@gmail.com>
2023-02-07 22:33:45 -0300

    Enumeracoes in JAVA. THANK YOU GOD!

---
+++ src/resumos/EnumeracaoComposica.java
@@ -0,0 +1,24 @@
+package resumos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EnumeracaoComposica {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		// ENUMERAÇÕES
+		
+		/*
+		 * 
+		 * 
+		 */
+		
+		
+		sc.close();
+	}
+
+}
