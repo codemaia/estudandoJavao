d969a951c2339091628d85cb034f81a52538c91e
Renan Maia <renanmaia.rl@gmail.com>
2023-02-07 22:07:09 -0300

    Data and hour in JAVA. THANK YOU GOD!

--- src/resumos/DataHora.java
+++ src/resumos/DataHora.java
@@ -3,6 +3,7 @@ package resumos;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
@@ -70,13 +71,19 @@ public class DataHora {
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
+		//Customizando uma data-hora "Instant"
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());   
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
+		
+		
 		LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 =  LocalDateTime.now();
 		Instant d03 = Instant.now();
 		
 		LocalDate d04 = LocalDate.parse("2023-02-07");
 		LocalDateTime d05 = LocalDateTime.parse("2023-02-07T09:30:30");
-		Instant d06 = Instant.parse("2023-02-07T09:30:30Z");
+		Instant d06 = Instant.parse("2023-02-08T01:30:30Z");
 		Instant d07 = Instant.parse("2023-02-07T09:30:30-03:00");
 		
 		LocalDate d08 = LocalDate.parse("01/01/2023", fmt1);
@@ -96,10 +103,38 @@ public class DataHora {
 		System.out.println("d10 = " + d10);
 		System.out.println("d11 = " + d11);	
 		
+		//imprimir data formatada / customizada	
+		System.out.println("d04 customizado = " + d04.format(fmt1));
+		//ou
+		System.out.println("ou d04 customizado = " + fmt1.format(d04));
+		
+		//customizando Instant
+		System.out.println("d06 customizado = " + fmt3.format(d06));
 		
 		
 		
+		// CONVERTENDO DATA-HORA GLOBAL PARA LOCAL
+
+		/**
+		 *  # converter daa-hora global para local necessita colocar 
+		 *  o TIMEZONE
+		 * 
+		 * 
+		 */
+		 
 		
+		LocalDateTime r1 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+//		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		System.out.println("r1 = " + r1);
+		
+		//pegar dia / mes / ano / hora / minuto
+		
+		System.out.println("d04 dia = " + d04.getDayOfMonth());
+		System.out.println("d04 mês = " + d04.getMonthValue());
+		System.out.println("d04 ano = " + d04.getYear());
+		
+		System.out.println("d05 hora = " + d05.getHour());
+		System.out.println("d06 minuto = " + d05.getMinute());
 		
 		
 		sc.close();
