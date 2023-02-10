a6aae3ff29df292f55835c4920eea2db08abd351
Renan Maia <renanmaia.rl@gmail.com>
2023-02-09 21:44:27 -0300

    studying enumeration in JAVA. THANKS MY GOD!

--- src/resumos/EnumeracaoComposica.java
+++ src/resumos/EnumeracaoComposica.java
@@ -79,6 +79,43 @@ public class EnumeracaoComposica {
 		 * 
 		 */
 		
+		
+		
+		
+		
+		// COMPOSIÇAO
+		
+		
+		/*
+		 * Composição
+		 * 
+		 * 	- É um tipo de associação que permite que um objeto contenha outro
+		 *  
+		 *  - Relação "tem-um" ou "tem-vários"
+		 *  
+		 *  
+		 *  # Vantagens
+		 *  	- Organização: divisão de responsabilidades
+		 *  	- Coesão
+		 *  	- Flexibilidade
+		 *  	- Reuso
+		 *  
+		 *  Nota: Embora o símbolo UML para composição (todo-parte) seja o diamante
+		 *  	  preto, neste contexto estamos chamando de composição qualquer associação tipo 
+		 *  	  "tem-um" e "tem-vários"
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
 		sc.close();
 	}
 
