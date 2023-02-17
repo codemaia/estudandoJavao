a590ea3330bd9a0c5cce6d2e63d3e4b4f79098dc
Renan Maia <renanmaia.rl@gmail.com>
2023-02-17 01:07:17 -0300

    I'm getting the hang of java. GOD IS GOOD!

--- src/application/ProgramPost.java
+++ src/application/ProgramPost.java
@@ -1,7 +1,12 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Comment;
+import entities.Post;
 
 public class ProgramPost {
 
@@ -11,6 +16,20 @@ public class ProgramPost {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		
+		Comment c1 = new Comment("Have a nice trip");
+		Comment c2 = new Comment("Wow that's awesome!"); 
+		
+		Post p1 = new Post(
+							LocalDateTime.parse("21/06/2018 13:05:44", formatter), 
+							"Traveling to New Zealand",
+							"I'm going to visit this wonderful country!",
+							12);
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		System.out.println(p1);
 		
 		
 		
--- src/entities/Post.java
+++ src/entities/Post.java
@@ -1,12 +1,15 @@
 package entities;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Post {
 	
-	private LocalDate moment;
+	public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+	
+	private LocalDateTime moment;
 	private String title;
 	private String contente;
 	private Integer likes;
@@ -19,7 +22,7 @@ public class Post {
 	}
 
 
-	public Post(LocalDate moment, String title, String contente, Integer likes) {
+	public Post(LocalDateTime moment, String title, String contente, Integer likes) {
 		this.moment = moment;
 		this.title = title;
 		this.contente = contente;
@@ -27,12 +30,12 @@ public class Post {
 	}
 
 
-	public LocalDate getMoment() {
+	public LocalDateTime getMoment() {
 		return moment;
 	}
 
 
-	public void setMoment(LocalDate moment) {
+	public void setMoment(LocalDateTime moment) {
 		this.moment = moment;
 	}
 
@@ -78,5 +81,24 @@ public class Post {
 	public void removeComment(Comment comment) {
 		comments.remove(comment);
 	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(title + "\n");
+		sb.append(likes + " Likes - ");
+		sb.append(formatter.format(moment) + "\n");
+		sb.append(contente + "\n");
+		sb.append("Comments: " + "\n");
+		for (Comment c : comments) {
+			sb.append(c.getText() + "\n");
+		}
+		
+		
+		return sb.toString();
+	}
+	
 	
 }
