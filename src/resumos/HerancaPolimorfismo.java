f46a5f99cd4c743f5d1b12e8a0d15fb07605f51b
Renan Maia <renanmaia.rl@gmail.com>
2023-02-20 23:46:16 -0300

    Herança e polimorfismo Java. GOD IS GOOD!

---
+++ src/resumos/HerancaPolimorfismo.java
@@ -0,0 +1,35 @@
+package resumos;
+
+import java.util.Scanner;
+
+public class HerancaPolimorfismo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		// HERANÇA
+		
+		/*
+		 * É um tipo de associação que permite que uma classe herde todos dados
+		 *  e comportamentos de outra
+		 *  
+		 *  Definiçoes importantes
+		 *  
+		 *  #Vantagens
+		 *   - Reuso
+		 *   - Polimorfismo
+		 *   
+		 *  #Sintaxe
+		 *   - class A extends B;
+		 * 
+		 */
+		
+		
+		sc.close();
+	}
+
+}
