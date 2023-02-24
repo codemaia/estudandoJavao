48a5985861d26ea816a3a09a19e05b18f8704525
Renan Maia <renanmaia.rl@gmail.com>
2023-02-23 23:16:28 -0300

    studying heranca and polimorfismo in Java. THANKS GOD!

--- src/application/ProgramEmployeeHeranca.java
+++ src/application/ProgramEmployeeHeranca.java
@@ -1,8 +1,11 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Employee;
 import entities.EmployeeHeranca;
 import entities.OutsourcedEmployee;
 
@@ -25,14 +28,57 @@ public class ProgramEmployeeHeranca {
 		System.out.println(emp2);
 		*/
 		
+		List<EmployeeHeranca> empList = new ArrayList<>();
+		
 		System.out.print("Enter the number of employees: ");
 		int n = sc.nextInt();
 		
 		for (int i = 1; i <= n; i++) {
 			System.out.println("Employee #" + i + " data:");
 			
+			System.out.print("Outsourced (y/n)? ");
+			char yesOrNo = sc.next().charAt(0);
+			
+			if (yesOrNo == 'n') {
+				
+				System.out.print("Name: ");
+				String name = sc.next();
+				
+				System.out.print("Hours: ");
+				Integer hours = sc.nextInt();
+				
+				System.out.print("Value per hour: ");
+				Double valuePerHour = sc.nextDouble();
+				
+				EmployeeHeranca emp = new EmployeeHeranca(name, hours, valuePerHour);
+				
+				empList.add(emp);
+			}
+			
+		
+			
+			if (yesOrNo == 'y') {
+				System.out.print("Name: ");
+				String name = sc.next();
+				
+				System.out.print("Hours: ");
+				Integer hours = sc.nextInt();
+				
+				System.out.print("Value per hour: ");
+				Double valuePerHour = sc.nextDouble();
+				
+				System.out.print("Additional charge: ");
+				Double additionalCharge = sc.nextDouble();
+				
+				EmployeeHeranca emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				
+				empList.add(emp);
+			}
+			
 			
 		}
+		
+		System.out.println(empList + "\n");
 		
 		sc.close();
 
