723805b7ca768dd3fad10d730d802e66e31da156
Renan Maia <renanmaia.rl@gmail.com>
2023-02-25 15:07:42 -0300

    studying heranca and polimorfismo in Java. THANKS GOD!

---
+++ src/applicationTwo/ImportedProduct.java
@@ -0,0 +1,31 @@
+package applicationTwo;
+
+public class ImportedProduct extends Product {
+	
+	private Double customFee;
+	
+	public ImportedProduct() {
+		super();
+	}
+
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;
+	}
+
+	public Double getCustomFee() {
+		return customFee;
+	}
+
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		//StringBuilder sb = new StringBuilder();
+		return name;
+		//return  + "(Custom fee: $ " + customFee + ")"; 
+	}
+	
+}
