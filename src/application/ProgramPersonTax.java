0f6ac321a8ac48c27d2cbf4fd588e026c021b7f1
Renan Maia <renanmaia.rl@gmail.com>
2023-03-02 08:36:35 -0300

    resolving exercise class and method absctract. GOD IS GOOD!

--- src/application/ProgramPersonTax.java
+++ src/application/ProgramPersonTax.java
@@ -17,12 +17,14 @@ public class ProgramPersonTax {
 		
 		
 		Person p1 = new IndividualPerson("Renan", 20000.0, 0.0);
-		Person p2 = new IndividualPerson("Livia", 50000.0, 2000.0);
-		Person p3 = new IndividualPerson("Bob", 120000.0, 1000.0);
+		Person p2 = new IndividualPerson("Renan Maia", 21000.0, 0.0);
+		Person p3 = new IndividualPerson("Livia", 50000.0, 2000.0);
+		Person p4 = new IndividualPerson("Bob", 120000.0, 1000.0);
 		
 		System.out.println(p1.getName() + " - " + p1.taxPaid());
 		System.out.println(p2.getName() + " - " + p2.taxPaid());
 		System.out.println(p3.getName() + " - " + p3.taxPaid());
+		System.out.println(p4.getName() + " - " + p4.taxPaid());
 		
 		sc.close();
 	}
--- src/entities_2/IndividualPerson.java
+++ src/entities_2/IndividualPerson.java
@@ -33,10 +33,13 @@ public class IndividualPerson extends Person{
 		if (getAnualIncome() <= 20000.0 && healthExpenditures == 0.0) {
 			tax = getAnualIncome() * 0.15;
 			
-		} else if (getAnualIncome() > 20000.0 && healthExpenditures > 0.0) {
-			tax = (getAnualIncome() * 0.25) - (healthExpenditures * 0.50);
+		} else if (getAnualIncome() > 20000.0 && healthExpenditures == 0.0) {
+			tax = getAnualIncome() * 0.25;
 			
-		} // falta adicionar mais uma condição
+		} else if (getAnualIncome() > 20000.0 && healthExpenditures > 0.0){
+			tax = (getAnualIncome() * 0.25) - (healthExpenditures * 0.50);    
+	
+		}
 		
 		return tax;
 		
