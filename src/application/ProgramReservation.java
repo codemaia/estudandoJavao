693fc0ce8b0f1ee8b7f476398183b76d1a9aca65
Renan Maia <renanmaia.rl@gmail.com>
2023-03-06 08:25:43 -0300

    created packges of exercise reservation. GOD IS GOOD EVERY DAY!

---
+++ src/application/ProgramReservation.java
@@ -0,0 +1,37 @@
+package application;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class ProgramReservation {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		String dt1 = "06/03/2023";
+		String dt2 = "16/03/2023";
+		
+		LocalDate dtCheckIn = LocalDate.parse(dt1, fmt);
+		LocalDate dtCheckOut = LocalDate.parse(dt2, fmt);
+		
+		
+		Reservation reservation = new Reservation(8080, dtCheckIn, dtCheckOut);
+		
+		System.out.println(reservation + " | Days reservation: " + reservation.duration());
+		
+		sc.close();
+		
+
+	}
+
+}
---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,67 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private LocalDate checkIn;
+	private LocalDate checkOut;
+	
+	private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public Reservation() {
+		
+	}
+
+
+	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+
+	public LocalDate getCheckIn() {
+		return checkIn;
+	}
+
+
+	public LocalDate getCheckOut() {
+		return checkOut;
+	}
+
+	
+	public Integer duration() {
+		Integer diff = checkOut.getDayOfMonth() - checkIn.getDayOfMonth();
+		return diff;
+	}
+
+
+	@Override
+	public String toString() {
+		return "roomNumber: " 
+				+ roomNumber
+				+ ", checkIn: " 
+				+ fmt.format(checkIn) 
+				+ ", checkOut: " 
+				+ fmt.format(checkOut);
+	}
+	
+	
+	
+
+	
+	
+}
