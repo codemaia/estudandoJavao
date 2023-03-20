7fc5ad752a902f67c01c749e7eee2f4ab1ef39e9
Renan Maia <renanmaia.rl@gmail.com>
2023-03-20 07:56:42 -0300

    add new chapter in Java. GOD IS GOOD!

---
+++ src/resumos/TrabalhandoComArquivos.java
@@ -0,0 +1,24 @@
+package resumos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TrabalhandoComArquivos {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
